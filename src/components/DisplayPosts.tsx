28ba1bf27e3587bb092af223d5b1d242a7205510
Nikita <49726724+nikitaagg1993@users.noreply.github.com>
2025-01-21 11:02:21 +0000

    display posts

---
+++ src/components/DisplayPosts.tsx
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { useQuery } from "react-query";
+
+const retrievePosts = async () => {
+  const response = await axios.get(
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+  return response.data;
+};
+export const DisplayPosts = () => {
+  const {
+    isLoading,
+    data: posts,
+    error,
+  } = useQuery("postsData", retrievePosts);
+
+  if (isLoading) return <div>Fetching posts...</div>;
+  if (error) return <div>An error occurred: {(error as {message: string})?.message}</div>;
+  return (
+    <ul>
+      {posts.map((post: any) => (
+        <li key={post.id}>{post.title}</li>
+      ))}
+    </ul>
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
 import App from "./App";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 const rootElement = document.getElementById("root")!;
 const root = ReactDOM.createRoot(rootElement);
 
+const queryClient = new QueryClient();
+
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
